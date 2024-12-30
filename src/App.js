eea76bfea1c7faf1015197ec9ab434e38288a8db
dkotsugai <dkotsugai@gmail.com>
2024-12-30 16:12:11 -0300

    Add initial project setup with Banner component and styling

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Banner from "./componentes/Banner/Banner";   // Importando o componente Banner
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />  {/* Renderizando o componente Banner */}
+    </div>
+  );
+}
+
+export default App;
