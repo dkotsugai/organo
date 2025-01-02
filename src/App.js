1593f93a7fe751923fb1de37447c32588f0a422f
dkotsugai <dkotsugai@gmail.com>
2025-01-02 04:13:54 -0300

    Add CampoTexto component and update App layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Banner from "./componentes/Banner/Banner";   // Importando o componente Banner
+import CampoTexto from "./componentes/CampoTexto"; // Importando o componente CampoTexto
+import Banner from "./componentes/Banner/"; // Importando o componente Banner
 
 function App() {
   return (
     <div className="App">
-      <Banner />  {/* Renderizando o componente Banner */}
+      <Banner /> {/* Renderizando o componente Banner */}
+      <CampoTexto label="Nome" placeholder="Digite seu nome" />
+      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+      <CampoTexto label="Imagem" placeholder="Digite o endereco da imagem" />
     </div>
   );
 }
--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,11 +1,13 @@
-import './Banner.css';
-function Banner() {
-    // JSX
-  return ( 
-    <header className="banner">
-  <img src="/imagens/banner.png" alt="o banner principal da página do Organo" /> 
-    </header>
-  )
-}
+import "./Banner.css";
 
-export default Banner;
+export const Banner = () => {
+  // JSX
+  return (
+    <header className="banner">
+      <img
+        src="/imagens/banner.png"
+        alt="o banner principal da página do Organo"
+      />
+    </header>
+  );
+};
